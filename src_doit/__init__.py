327eeb8c9aa2bbe3ea9907be7025bac18973b03b
bozo.kopic <bozo@kopic.xyz>
2021-08-08 18:32:55 +0200

    add tests

--- src_doit/__init__.py
+++ src_doit/__init__.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import subprocess
+import sys
 
 from hat.doit import common
 
@@ -8,11 +10,14 @@ from . import sqlite3
 
 __all__ = ['task_clean_all',
            'task_build',
+           'task_check',
+           'task_test',
            *sqlite3.__all__]
 
 
 build_dir = Path('build')
 src_py_dir = Path('src_py')
+pytest_dir = Path('test_pytest')
 
 
 def task_clean_all():
@@ -40,3 +45,29 @@ def task_build():
 
     return {'actions': [build],
             'task_dep': ['sqlite3']}
+
+
+def task_check():
+    """Check with flake8"""
+    return {'actions': [(_run_flake8, [src_py_dir]),
+                        (_run_flake8, [pytest_dir])]}
+
+
+def task_test():
+    """Test"""
+
+    def run(args):
+        subprocess.run([sys.executable, '-m', 'pytest',
+                        '-s', '-p', 'no:cacheprovider',
+                        *(args or [])],
+                       cwd=str(pytest_dir),
+                       check=True)
+
+    return {'actions': [run],
+            'pos_arg': 'args',
+            'task_dep': ['sqlite3']}
+
+
+def _run_flake8(path):
+    subprocess.run([sys.executable, '-m', 'flake8', str(path)],
+                   check=True)
